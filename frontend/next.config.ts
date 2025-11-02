5fe02d48170280480058fc369e44a18e210a11c2
Avoisavo <lvera296@gmail.com>
2025-11-02 12:24:39 +0800

    debug

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -10,6 +10,20 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  transpilePackages: ['@splinetool/react-spline', '@splinetool/runtime'],
+  experimental: {
+    turbo: {
+      resolveExtensions: [
+        '.mdx',
+        '.tsx',
+        '.ts',
+        '.jsx',
+        '.js',
+        '.mjs',
+        '.json',
+      ],
+    },
+  },
 };
 
 export default nextConfig;
